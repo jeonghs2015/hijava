ea2103d0ae17cf7ea0f818fc78f7e5d947b62756
jeonghs <hsjung201558@gmail.com>
2022-04-08 22:25:47 +0900

    재귀 함수(Recursive Method)를 사용하여 피보나치 수열 계산 하기

---
+++ src/hijava/practice/Fibonacci.java
@@ -0,0 +1,23 @@
+package hijava.practice;
+
+public class Fibonacci {
+
+    public static void main(String[] args) {
+        int inum = 10;
+        for(int i = 0;i <= inum;i++){
+            System.out.print(fibo(i) + "  ");
+
+        }
+
+    }
+    private static int fibo(int num){
+        // 1번 방법
+        if(num <= 1) return num;
+        return fibo(num - 1) + fibo(num - 2);
+        // 2번 방법
+        // if(num > 1)
+        //  return fibo(num - 1) + fibo(num - 2);
+        // else
+        //  return num;
+    }
+}
