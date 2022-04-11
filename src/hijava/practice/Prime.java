ad109f8e55c107f544f45581529d2eb6fd42e6a4
jeonghs <hsjung201558@gmail.com>
2022-04-11 23:34:03 +0900

    ArrayList를 이용한 소수의 합

--- src/hijava/practice/Prime.java
+++ src/hijava/practice/Prime.java
@@ -1,15 +1,17 @@
 package hijava.practice;
 
+import java.util.ArrayList;
+
 public class Prime {
     public static void main(String[] args) {
         int total = 0;
 
         for(int num = 2; num <= 100; num++){
 
-            if (isPrime(num))
+            if (isPrime(num)) {
                 total += num;
-            else
-                System.out.println(num + " is Not Prime Number!!");
+                //primeList.add(num);
+            }
 
         }
 
@@ -17,12 +19,16 @@ public class Prime {
     }
 
     //소수를 찾아내는 함수
+    private static ArrayList<Integer> primeList = new ArrayList<>();
+
     private static boolean isPrime(int num) {
-        for(int j = 2; j < num; j++){
+     //   for(int j = 2; j < num; j++){
+        for(int j : primeList){
             if(num % j == 0){
                 return false;
             }
         }
+        primeList.add(num);
         return true;
     }
 }
